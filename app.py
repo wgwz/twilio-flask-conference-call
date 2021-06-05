c88be9196e98e1caee6280005c7bd2e33e9e90bb
Kyle Lawlor-Bagcal <wgwz@pm.me>
2021-06-04 20:33:14 -0400

    add proof-of-concept security pin to the conference call

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
 from flask import Flask, Response, request
-from twilio.twiml.voice_response import VoiceResponse, Dial
+from twilio.twiml.voice_response import Dial, Gather, VoiceResponse
 
 
 app = Flask(__name__)
 
 
-@app.route("/conference", methods=["GET", "POST"])
+@app.route("/conference", methods=["POST"])
 def conference():
     print(f"{request.form=}")
     response = VoiceResponse()
-    dial = Dial()
-    dial.conference(
-        "Twilio Test Conference",
-        status_callback="/events",
-        status_callback_event="start end join leave mute hold",
-    )
-    response.append(dial)
+    pin_code = request.form.get("Digits")
+    if pin_code and pin_code == "12345":
+        dial = Dial()
+        dial.conference(
+            "Twilio Test Conference",
+            status_callback="/events",
+            status_callback_event="start end join leave mute hold",
+        )
+        response.append(dial)
+    else:
+        gather = Gather(action="/conference")
+        gather.say("Please enter your conference pin,\nfollowed by the pound sign")
+        response.append(gather)
     return Response(str(response), mimetype="text/xml")
 
 
